6c9c1816751fe4a14f957c14ddabc7fad0d9e2fc
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-03 13:54:15 +0530

    all commit

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,55 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*sdasdfdasfsdfsd*/
+void sortedSquares(int* nums, int size, int* result) {
+    int left = 0, right = size - 1, pos = size - 1;
 
-
-int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    *returnSize = numsSize; 
-
-    int* result = (int*)malloc(numsSize * sizeof(int));  // Allocate memory for result array
-    if (!result) return NULL;  // Return NULL if allocation fails
-
-    int left = 0, right = numsSize - 1;
-    int index = numsSize - 1;
-
-  
     while (left <= right) {
-        int leftSquare = nums[left] * nums[left];
-        int rightSquare = nums[right] * nums[right];
-
-        if (leftSquare > rightSquare) {
-            result[index--] = leftSquare;
+        if (abs(nums[left]) > abs(nums[right])) {
+            result[pos] = nums[left] * nums[left];
             left++;
         } else {
-            result[index--] = rightSquare;
+            result[pos] = nums[right] * nums[right];
             right--;
         }
-    }/*now lets make it*/ /*jfsjdfkjsdkfkjds*/
-
-    return result;
-}
-
-
-void printArray(int* arr, int size) {
-    printf("[");
-    for (int i = 0; i < size; i++) {
-        printf("%d", arr[i]);
-        if (i < size - 1) printf(", ");
+        pos--;
     }
-    printf("]\n");
-}
-
-
-int main() {
-    int nums[] = {-4, -1, 0, 3, 10};
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int returnSize;
-
-    int* result = sortedSquares(nums, numsSize, &returnSize);
-
-    printf("Sorted Squares: ");
-    printArray(result, returnSize);
-
-    free(result);  // Free the allocated memory
-    return 0;
 }
